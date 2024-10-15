981b2eaa7085075832927c131bcbc96898f1a065
skeh0003 <skeh0003@student.monash.edu>
2024-10-15 23:17:00 +1100

    Finished rest of visualisation

--- week_10_hw.js
+++ week_10_hw.js
@@ -1,3 +1,8 @@
+var vg_gdp_world = "./country_gdp.json";
+vegaEmbed("#countrygdp", vg_gdp_world).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
 var vg_1 = "./week 8 hw.json";
 vegaEmbed("#worldmap", vg_1).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
@@ -15,5 +20,15 @@ vegaEmbed("#industrybar", vg_state_industries).then(function(result) {
 
 var vg_gdp_change = "./gdp_line_chart.json";
 vegaEmbed("#gdpline", vg_gdp_change).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_labour = "./labour_cost.json";
+vegaEmbed("#labourline", vg_labour).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_tax = "./taxes.json";
+vegaEmbed("#taxline", vg_tax).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
